2fd0ba424b060bc66d4f58b1847a0c9e9ecd6dad
Ningluosi <xu_zhang1108@163.com>
2024-12-23 15:16:09 +0000

    restore hyperloglog_test.cpp

--- test/primer/hyperloglog_test.cpp
+++ test/primer/hyperloglog_test.cpp
@@ -69,7 +69,7 @@ TEST(HyperLogLogTest, BasicTest2) {
     if (i == 0) {
       obj.ComputeCardinality();
       ans = obj.GetCardinality();
-      ASSERT_EQ(ans, 7);
+      ASSERT_EQ(ans, 10);
     }
   }
 
